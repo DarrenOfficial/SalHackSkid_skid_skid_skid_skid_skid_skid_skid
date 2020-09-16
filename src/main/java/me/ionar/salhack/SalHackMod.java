da65bf151445f41e53b2c9b4bda7e76c4d5e3a28
natan <66911017+natan515@users.noreply.github.com>
2020-09-16 20:59:33 +0300

    Update SalHackMod.java

--- src/main/java/me/ionar/salhack/SalHackMod.java
+++ src/main/java/me/ionar/salhack/SalHackMod.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = "salhack", name = "SalHack", version = SalHackMod.VERSION)
 public final class SalHackMod
+    //why don't u change it to salhackskid?
 {
     public static final String NAME = "SalHackSkid";
     public static final String VERSION = "1.1.3d";
